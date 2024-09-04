2ed0a4eda6fc5a8cf3ea059fc03abd00601ef4c4
Patrick Braeutigam <patrick.braeutigam@argetp21.de>
2024-09-04 15:12:07 +0200

    + disclaimer

---
+++ plot3.R
@@ -0,0 +1,18 @@
+#plot3.R
+# Copyright (C) [2015] [patbr21]
+# All rights reserved.
+
+# This script is part of a public GitHub project and is subject to the following terms:
+#
+# 1. Permission is granted to use, copy, modify, and distribute this code for any purpose, 
+#    provided that the original author ([Your Name or Username]) is clearly credited.
+#
+# 2. Commercial use of this code is strictly prohibited without prior written permission 
+#    from the author.
+#
+# 3. This code is provided "as is", without warranty of any kind, express or implied. The author 
+#    is not liable for any damages or losses that may arise from the use of this code.
+#
+# 4. By using this code, you agree to these terms.
+
+#--------
